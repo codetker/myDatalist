8e95fe573d8d094091b88cfba296e5835391d299
codetker <925639912@qq.com>
2015-07-24 17:22:51 +0800

    bugfix:keyboard do not follow cursor mouseover;code simplied

--- js/jquery.codetker.datalist.js
+++ js/jquery.codetker.datalist.js
@@ -12,9 +12,9 @@
 	var Datalist = function(ele, opt) {
 			this.$element = ele;
 			this.defaults = {
-					'bgcolor': 'green',
-					'widths': 1,
-					'heights': 1
+					'bgcolor': '#eee',
+					'bgcolorHover': '#ccc',
+					'fixKeyboard': true
 				},
 				this.options = $.extend({}, this.defaults, opt);
 		}
@@ -22,91 +22,98 @@
 	Datalist.prototype = {
 			showList: function() {
 				var
-					color = this.options.bgcolor,
-					width = this.options.widths,
-					height = this.options.heights, //属性值
+					bgcolor = this.options.bgcolor,
+					bgcolorHover = this.options.bgcolorHover,
+					fixkeyboard = this.options.fixKeyboard,
 					obj = this.$element, //obj为最外层包裹的div之类的元素，应该拥有positive：relative属性,方便datalist定位。
 					input = $(obj).children().eq(0), //input元素
-					inputUl = $(obj).children().eq(1); //datalist元素
+					inputUl = $(obj).children().eq(1), //datalist元素
+					n = -1; //记录鼠标位置,-1表示未选中。当n=-1时直接按enter浏览器默认为倒数第一个
 
-				//设置弹出datalist的大小和样式
-				$(inputUl).css({
-					"top": $(input).outerHeight() + "px",
-					"width": $(input).outerWidth() * width + "px"
-				});
-				$(inputUl).children().css({
-					"width": $(input).outerWidth() * width + "px",
-					"height": $(input).outerHeight() * height + "px"
-				});
-				/*$(inputUl).children('li').hover(function() {
-				    $(this).css("background-color",color);
-				}, function() {
-				    $(this).css("background-color","#fff");
-				});*/
-				$(inputUl).children('li').mouseover(function() {
-					$(this).css("background-color", color);
-					$(this).siblings().css("background-color", "#fff");
-				});
-				$(inputUl).children('li').mouseout(function() {
-					$(this).css("background-color", "#fff");
-				});
+				function setCss() {
+					//设置弹出datalist的大小和样式
+					$(inputUl).css({
+						"top": $(input).outerHeight() + "px",
+						"width": $(input).outerWidth() + "px"
+					});
+					$(inputUl).children().css({
+						"width": $(input).outerWidth() + "px",
+						"height": $(input).outerHeight() + "px",
+						"background-color": bgcolor
+					});
+
+					$(inputUl).children('li').mouseover(function() {
+						n = $(this).index();
+						$(this).css("background-color", bgcolorHover);
+						$(this).siblings().css("background-color", bgcolor);
+					});
+					$(inputUl).children('li').mouseout(function() {
+						$(this).css("background-color", bgcolor);
+					});
+				}
+
+				function fixKeyBoard() {
+					if (fixkeyboard) {
+						$(document).keydown(function(event) {
+							/* 点击键盘上下键，datalist变化 */
+							stopDefaultAndBubble(event);
+
+							switch (event.keyCode) {
+								case 38: //向上
+									if (n == 0 || n == -1) {
+										n = 4;
+									} else {
+										n--;
+									}
+									$(inputUl).children('li').eq(n).siblings().mouseout();
+									$(inputUl).children('li').eq(n).mouseover();
+									break;
+								case 40: //向下
+									if (n == 4) {
+										n = 0;
+									} else {
+										n++;
+									}
+									$(inputUl).children('li').eq(n).siblings().mouseout();
+									$(inputUl).children('li').eq(n).mouseover();
+									break;
+								case 13: //enter
+									$(inputUl).children('li').eq(n).mouseout();
+									$(input).val($(inputUl).children('li').eq(n).text());
+									n = -1;
+									break;
+								default:
+									break;
+							}
+
+						});
+					}
+				}
+
+				function stopDefaultAndBubble(e) {
+					e = e || window.event;
+					//阻止默认行为
+					if (e.preventDefault) {
+						e.preventDefault();
+					}
+					e.returnValue = false;
+
+					//阻止冒泡
+					if (e.stopPropagation) {
+						e.stopPropagation();
+					}
+					e.cancelBubble = true;
+				}
+
+				setCss();
 				//再次focus变为空，也可以改为某个默认值
 				//datalist的显示和隐藏
 				$(input).focus(function() {
-					if ($(this).val() != "") {
-						$(this).val("");
+					if ($(this).val() != '') {
+						$(this).val('');
 					}
 					$(inputUl).slideDown(500);
-					/*
-					 * 改进：仅在focus的时候触发,增强用户体验
-					 * 1.绑定上下按钮事件
-					 * 2.绑定enter事件
-					 * 3.去掉默认属性
-					 */
-					var n = -1; //记录位置,-1表示未选中。当n=-1时直接按enter浏览器默认为倒数第一个
-					$(document).keydown(function(event) {
-						/* 点击键盘上下键，datalist变化 */
-						stopDefaultAndBubble(event);
-						if (event.keyCode == 38) { //向上按钮
-							if (n == 0 || n == -1) {
-								n = 4;
-							} else {
-								n--;
-							}
-							$(inputUl).children('li').eq(n).siblings().mouseout();
-							$(inputUl).children('li').eq(n).mouseover();
-						} else if (event.keyCode == 40) { //向上按钮
-							if (n == 4) {
-								n = 0;
-							} else {
-								n++;
-							}
-							$(inputUl).children('li').eq(n).siblings().mouseout();
-							$(inputUl).children('li').eq(n).mouseover();
-						} else if (event.keyCode == 13) { //enter键
-							$(inputUl).children('li').eq(n).mouseout();
-							$(input).val($(inputUl).children('li').eq(n).text());
-							n = -1;
-						}
-					});
-
-
-					//去掉浏览器默认
-					function stopDefaultAndBubble(e) {
-						e = e || window.event;
-						//阻止默认行为
-						if (e.preventDefault) {
-							e.preventDefault();
-						}
-						e.returnValue = false;
-
-						//阻止冒泡
-						if (e.stopPropagation) {
-							e.stopPropagation();
-						}
-						e.cancelBubble = true;
-					}
-
+					fixKeyBoard();
 				});
 				$(input).blur(function() {
 					$(inputUl).slideUp(500);
